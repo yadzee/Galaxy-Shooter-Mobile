5037d23f8b6166d4f12ba170bb0ee1100ee8aa8f
Ian Firsov <Diatonly@gmail.com>
2021-02-24 13:10:57 +0300

    Ready to build (Android)

---
+++ Assets/Scripts/Joybutton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Joybutton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    public bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+
+}
